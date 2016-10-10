fcf66229d659159c4db23d386802a13c0c494064
blue0knight <oni_rivera98@yahoo.com>
2016-10-10 15:32:55 -0400

    fixed avatar not showing

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,8 @@ module UsersHelper
 
   # returns gravatar of current user
   def gravatar_for(user)
-    gravatar_id = "111d68d06e2d317b5a59c2c6c5bad808"
-    gravatar_url = "https://secure.gravatar.com/avatar/gravatar_id"
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
