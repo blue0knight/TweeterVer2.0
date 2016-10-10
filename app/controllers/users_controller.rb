2aa0b23401ac38f86b54d992a921163a9ca27ed2
blue0knight <oni_rivera98@yahoo.com>
2016-10-10 14:56:06 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,22 @@ class UsersController < ApplicationController
   end
 
   def new
+     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to Tweeter Ver. 2.0!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+  end
+
 end
